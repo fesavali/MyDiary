5c9b447fb281060246518f114fe8c8bbe3fcefe3
fesavali <felixnzioki99@gmail.com>
2021-12-09 09:39:24 +0300

    Remove unwanted code and fragments

--- app/src/main/java/com/savaliscodes/mydiary/MainActivity.kt
+++ app/src/main/java/com/savaliscodes/mydiary/MainActivity.kt
@@ -24,9 +24,6 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -50,9 +47,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
