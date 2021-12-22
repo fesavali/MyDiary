2dc5e8ee31738c5c29f483ef7937165c1555fa24
fesavali <felixnzioki99@gmail.com>
2021-12-22 16:04:41 +0300

    finish up on rc adapter and reading data

--- app/src/main/java/com/savaliscodes/mydiary/DiaryData.kt
+++ app/src/main/java/com/savaliscodes/mydiary/DiaryData.kt
@@ -1,5 +1,6 @@
 package com.savaliscodes.mydiary
-
+//simply initialise all data you need from the database
+//NB must be nullable
 data class DiaryData(var DiaryLogID: String ?= null, var UserId: String ?= null,
                      var LogTitle:String ?= null, var LogContents: String ?= null,
                      var LogTime: String ?= null)
