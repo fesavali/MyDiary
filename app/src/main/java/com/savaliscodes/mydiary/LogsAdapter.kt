ef6d503c0493a5106c32e1e99fdcd35cf420e257
fesavali <felixnzioki99@gmail.com>
2021-12-22 16:04:59 +0300

    finish up on rc adapter and reading data

--- app/src/main/java/com/savaliscodes/mydiary/LogsAdapter.kt
+++ app/src/main/java/com/savaliscodes/mydiary/LogsAdapter.kt
@@ -6,29 +6,34 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+//class adapter must contain a dataclass arraylist
 class LogsAdapter(private val LogsList : ArrayList<DiaryData>) : RecyclerView.Adapter<LogsAdapter.LogsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LogsAdapter.LogsViewHolder {
+        //inflate recyclerview with custom data view
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.custom_rcv, parent, false)
-
+        //return custom viewholder
         return LogsViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: LogsAdapter.LogsViewHolder, position: Int) {
-
+        //initialise data class and array position
         val diaryData : DiaryData = LogsList[position]
-
+        //bind data to views in custom viewholder
         holder.lTitle.text = diaryData.LogTitle
         holder.lContents.text = diaryData.LogContents
         holder.lTime.text = diaryData.LogTime
     }
 
     override fun getItemCount(): Int {
+       //return a arraylist size of the data class
        return LogsList.size
     }
 
+     //initialise a Viewholder class that extends recyclerview.viewholder
     public class LogsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+         //get ref to all views in custom view
         val lTitle : TextView = itemView.findViewById(R.id.log_title)
         val lContents : TextView = itemView.findViewById(R.id.log_contents)
         val lTime : TextView = itemView.findViewById(R.id.log_time)
