0df9d4ccce13addab44fddc37a2f83b46ed2f477
fesavali <felixnzioki99@gmail.com>
2021-12-23 15:40:34 +0300

    Refresh recyclerview when new note is added

--- app/src/main/java/com/savaliscodes/mydiary/LogsAdapter.kt
+++ app/src/main/java/com/savaliscodes/mydiary/LogsAdapter.kt
@@ -7,19 +7,20 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 //class adapter must contain a dataclass arraylist
-class LogsAdapter(private val LogsList : ArrayList<DiaryData>) : RecyclerView.Adapter<LogsAdapter.LogsViewHolder>() {
+class LogsAdapter(private val logsList : ArrayList<DiaryData>) : RecyclerView.Adapter<LogsAdapter.LogsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LogsAdapter.LogsViewHolder {
         //inflate recyclerview with custom data view
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.custom_rcv, parent, false)
+
         //return custom viewholder
         return LogsViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: LogsAdapter.LogsViewHolder, position: Int) {
         //initialise data class and array position
-        val diaryData : DiaryData = LogsList[position]
+        val diaryData : DiaryData = logsList[position]
         //bind data to views in custom viewholder
         holder.lTitle.text = diaryData.LogTitle
         holder.lContents.text = diaryData.LogContents
@@ -28,7 +29,7 @@ class LogsAdapter(private val LogsList : ArrayList<DiaryData>) : RecyclerView.Ad
 
     override fun getItemCount(): Int {
        //return a arraylist size of the data class
-       return LogsList.size
+       return logsList.size
     }
 
      //initialise a Viewholder class that extends recyclerview.viewholder
