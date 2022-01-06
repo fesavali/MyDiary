5825420b7b6d4bb09526d140d2e5181ccc12cbdc
fesavali <felixnzioki99@gmail.com>
2022-01-06 14:03:20 +0300

    update ui for del account

--- app/src/main/java/com/savaliscodes/mydiary/Register.kt
+++ app/src/main/java/com/savaliscodes/mydiary/Register.kt
@@ -26,6 +26,13 @@ class Register : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        val toast = intent.getStringExtra("del")
+        val name = intent.getStringExtra("name")
+        if(112.equals(toast)){
+            Toast.makeText(this, "Its sad to see you leave.$name" +
+                    "Would you like to create another Account?", Toast.LENGTH_LONG).show()
+        }
+
         val regBtn = findViewById<Button>(R.id.btnReg)
 //        val googleSign = findViewById<Button>(R.id.googleBtn)
         //register User
@@ -108,5 +115,9 @@ class Register : AppCompatActivity() {
         private const val TAG = "GoogleActivity"
         private const val RC_SIGN_IN = 9001
     }
+
+    override fun onBackPressed() {
+        finish()
     }
+}
 
