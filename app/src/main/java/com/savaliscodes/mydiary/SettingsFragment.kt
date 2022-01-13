970e4637cba64286f0b8b289a74116c3afeb172b
fesavali <felixnzioki99@gmail.com>
2022-01-13 16:39:15 +0300

    get ref from settings

--- app/src/main/java/com/savaliscodes/mydiary/SettingsFragment.kt
+++ app/src/main/java/com/savaliscodes/mydiary/SettingsFragment.kt
@@ -1,11 +1,15 @@
 package com.savaliscodes.mydiary
 
+
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        setPreferencesFromResource(R.xml.root_preferences, prefName)
+    }
+    companion object{
+        const val prefName = "SHARED_PREF"
     }
 }
