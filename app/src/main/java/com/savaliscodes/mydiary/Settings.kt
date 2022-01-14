1b19e8978d6317246603f24fbe9fdb7f72eccc28
fesavali <felixnzioki99@gmail.com>
2022-01-14 15:33:34 +0300

    configure app to user preferences

--- app/src/main/java/com/savaliscodes/mydiary/Settings.kt
+++ app/src/main/java/com/savaliscodes/mydiary/Settings.kt
@@ -1,12 +1,17 @@
 package com.savaliscodes.mydiary
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.savaliscodes.mydiary.databinding.ActivitySettingsBinding
+import kotlin.system.exitProcess
 
 class Settings : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
+
+    lateinit var fromMain : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +21,19 @@ class Settings : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+        fromMain = intent.getStringExtra("close").toString()
+
         //set onclick listener for nav icon
         binding.toolbar.setNavigationOnClickListener {
-           finish()
+//           val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+            if(2.equals(fromMain)){
+                finishAffinity()
+                System.exit(0)
+            }else{
+                Toast.makeText(this, "Your Preferences will Update on Next Launch", Toast.LENGTH_SHORT).show()
+                finish()
+            }
         }
         //Declaring fragment manager from making data
         // transactions using the custom fragment
@@ -30,4 +45,14 @@ class Settings : AppCompatActivity() {
         mFragment.arguments = mBundle
         mFragmentTransaction.add(R.id.frameLayout, mFragment).commit()
     }
+
+    override fun onBackPressed() {
+        if(2.equals(fromMain)){
+            finishAffinity()
+            exitProcess(0)
+        }else{
+            finish()
+        }
+    }
+
 }
